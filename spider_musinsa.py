4fba0cc31b921ffdd7114dd53a9a333708e1c3c8
ndgndg91 <ndgndg91@gmail.com>
2016-09-27 00:12:58 +0900

    addspider_musinsa.py

--- spider_musinsa.py
+++ spider_musinsa.py
@@ -12,6 +12,7 @@ def spider(max_pages):
 		soup1=BeautifulSoup(f,'html.parser')
 		all_table=soup.find('table',class_='table_th_grey')
 		trtr=all_table.find_all('tr')
+		list(trtr)
 		trtr_row_col = str(trtr[0].find_all('th'))
 		p = re.compile('[a-z]|[A-Z]|\W|\d')
 		alphaBet = p.findall(trtr_row_col)
@@ -61,63 +62,73 @@ def spider(max_pages):
 		row2 = trtr_row_th2 + trtr_row_td2
 		print(row2)
 #################################################################
-		trtr_row_th3 = str(trtr[5].find_all('th'))
-		p = re.compile('[a-z]|[A-Z]|\W|')
-		alphaBet = p.findall(trtr_row_th3)
+		if trtr[5] in globals():
+			trtr_row_th3 = str(trtr[5].find_all('th'))
+			p = re.compile('[a-z]|[A-Z]|\W|')
+			alphaBet = p.findall(trtr_row_th3)
 
-		for i in alphaBet:
-			trtr_row_th3 = trtr_row_th3.replace(i,'')
+			for i in alphaBet:
+				trtr_row_th3 = trtr_row_th3.replace(i,'')
 #print(trtr_row_th3)
 #************************************************************#
-		trtr_row_td3 = str(trtr[5].find_all('td'))
-		p = re.compile('[a-z]|[A-Z]|\s|<|>|_|/|"|=')
-		alphaBet = p.findall(trtr_row_td3)
+			trtr_row_td3 = str(trtr[5].find_all('td'))
+			p = re.compile('[a-z]|[A-Z]|\s|<|>|_|/|"|=')
+			alphaBet = p.findall(trtr_row_td3)
 
-		for i in alphaBet:
-			trtr_row_td3 = trtr_row_td3.replace(i,'')
+			for i in alphaBet:
+				trtr_row_td3 = trtr_row_td3.replace(i,'')
 #print(trtr_row_td3)
 
-		row3 = trtr_row_th3 + trtr_row_td3
-		print(row3)
+			row3 = trtr_row_th3 + trtr_row_td3
+			print(row3)
+		else:
+			break
 #########################################################
-		trtr_row_th4 = str(trtr[6].find_all('th'))
-		p = re.compile('[a-z]|[A-Z]|\W|')
-		alphaBet = p.findall(trtr_row_th4)
+		if trtr[6]in globals():
+			trtr_row_th4 = str(trtr[6].find_all('th'))
+			p = re.compile('[a-z]|[A-Z]|\W|')
+			alphaBet = p.findall(trtr_row_th4)
 
-		for i in alphaBet:
-			trtr_row_th4 = trtr_row_th4.replace(i,'')
+			for i in alphaBet:
+				trtr_row_th4 = trtr_row_th4.replace(i,'')
 #print(trtr_row_th4)
 #************************************************************#
-		trtr_row_td4 = str(trtr[6].find_all('td'))
-		p = re.compile('[a-z]|[A-Z]|\s|<|>|_|/|"|=')
-		alphaBet = p.findall(trtr_row_td4)
+			trtr_row_td4 = str(trtr[6].find_all('td'))
+			p = re.compile('[a-z]|[A-Z]|\s|<|>|_|/|"|=')
+			alphaBet = p.findall(trtr_row_td4)
 
-		for i in alphaBet:
-			trtr_row_td4 = trtr_row_td4.replace(i,'')
+			for i in alphaBet:
+				trtr_row_td4 = trtr_row_td4.replace(i,'')
 #print(trtr_row_td4)
 
-		row4 = trtr_row_th4 + trtr_row_td4
-		print(row4)
+			row4 = trtr_row_th4 + trtr_row_td4
+			print(row4)
+		else:
+			break
 ############################################################
-		trtr_row_th5 = str(trtr[7].find_all('th'))
-		p = re.compile('[a-z]|[A-Z]|\W|')
-		alphaBet = p.findall(trtr_row_th5)
+		if trtr[7] in globals():
+			trtr_row_th5 = str(trtr[7].find_all('th'))
+			p = re.compile('[a-z]|[A-Z]|\W|')
+			alphaBet = p.findall(trtr_row_th5)
 
-		for i in alphaBet:
-			trtr_row_th5 = trtr_row_th5.replace(i,'')
+			for i in alphaBet:
+				trtr_row_th5 = trtr_row_th5.replace(i,'')
 #print(trtr_row_th5)
 #************************************************************#
-		trtr_row_td5 = str(trtr[7].find_all('td'))
-		p = re.compile('[a-z]|[A-Z]|\s|<|>|_|/|"|=')
-		alphaBet = p.findall(trtr_row_td5)
+			trtr_row_td5 = str(trtr[7].find_all('td'))
+			p = re.compile('[a-z]|[A-Z]|\s|<|>|_|/|"|=')
+			alphaBet = p.findall(trtr_row_td5)
 
-		for i in alphaBet:
-			trtr_row_td5 = trtr_row_td5.replace(i,'')
+			for i in alphaBet:
+				trtr_row_td5 = trtr_row_td5.replace(i,'')
 #print(trtr_row_td4)
 
-		row5 = trtr_row_th5 + trtr_row_td5
-		print(row5)
+			row5 = trtr_row_th5 + trtr_row_td5
+			print(row5)
+			page +=1
+		else:
+			break
 ############################################################
-		page +=1
+		
 
 spider(358865)
