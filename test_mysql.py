24fedd555a7fd66109f8fbbcdfd0fd26539ed9a8
ndgndg91 <ndgndg91@gmail.com>
2016-10-02 23:52:23 +0900

    connmysqlandinsert

--- test_mysql.py
+++ test_mysql.py
@@ -1,3 +1,16 @@
+import csv
 import pymysql
+#pymysql
+conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='ehdrlf123^^', db='search_for_cloth') 
+cursor = conn.cursor()
+#cursor.execute("select * from search_pants")
+csv_data = csv.reader(open('299432.csv'))
 
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='ehdrlf123^^', db='search_for_cloth')
+for row in csv_data:
+	cursor.execute("INSERT INTO search_pants VALUES(%s,%s,%s,%s,%s,%s)", row)
+
+#for r in cursor:
+#    print(r)		
+conn.commit()
+cursor.close()
+print('Done')
