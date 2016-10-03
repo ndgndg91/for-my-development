309b59240ec1f44cdd95b9c2b37e9c8468ef481f
ndgndg91 <ndgndg91@gmail.com>
2016-10-03 13:17:44 +0900

    connmysqlinsertpossible

--- test_mysql.py
+++ test_mysql.py
@@ -1,16 +1,22 @@
 import csv
 import pymysql
-#pymysql
-conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='ehdrlf123^^', db='search_for_cloth') 
-cursor = conn.cursor()
-#cursor.execute("select * from search_pants")
-csv_data = csv.reader(open('299432.csv'))
 
-for row in csv_data:
-	cursor.execute("INSERT INTO search_pants VALUES(%s,%s,%s,%s,%s,%s)", row)
+def insertdata(max_pages):
+	page = 299432
+	while page < max_pages:
+		conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='ehdrlf123^^', db='search_for_cloth') 
+		cursor = conn.cursor()
+		#cursor.execute("select * from search_pants")
+		csv_data = csv.reader(open(str(page)+'.csv'))
 
-#for r in cursor:
-#    print(r)		
-conn.commit()
-cursor.close()
-print('Done')
+		for row in csv_data:
+			cursor.execute("INSERT INTO search_pants VALUES(%s,%s,%s,%s,%s,%s)", row)
+
+		#for r in cursor:
+		#    print(r)		
+		conn.commit()
+		cursor.close()
+		page+=1
+		print('Done')
+
+insertdata(299433)
