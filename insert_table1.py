66fc8f6e34cb7ebe38e1fa21e6d77230d8cf98cf
ndgndg91 <ndgndg91@gmail.com>
2016-10-12 22:26:08 +0900

    more_complete

--- insert_table1.py
+++ insert_table1.py
@@ -10,9 +10,12 @@ def insertdata_table1(max_pages):
 	#cursor.execute("select * from search_pants")
 	csv_data = csv.reader(open(str(fixpage)+'~'+str(max_pages-1)+'table1.csv'))
 
+
+	cursor.execute("create table z_table1 (PantsNo int,  Img_URL varchar(500),  Link varchar(500));")
 	for row in csv_data:
 		cursor.execute("INSERT INTO z_table1(Img_URL,Link) VALUES(%s,%s)", row)
 	cursor.execute("UPDATE z_table1 SET z_table1.Pantsno=substr(link,45,6);")
+	cursor.execute("ALTER TABLE z_table1 ADD CONSTRAINT z_table1_PK_PantsNo PRIMARY KEY(PantsNo);")
 
 	#for r in cursor:
 	#    print(r)		
--- insert_table2.py
+++ insert_table2.py
@@ -10,9 +10,13 @@ def insertdata_table2(max_pages):
 	#cursor.execute("select * from search_pants")
 	csv_data = csv.reader(open(str(fixpage)+'~'+str(max_pages-1)+'table2.csv'))
 
+	cursor.execute("create table z_pants_musinsa (  PantsNo int, Size int, Length int, Waist int, Thigh  int, Crotch int, Hem int, Link varchar(200));")
+
 	for row in csv_data:
 		cursor.execute("INSERT INTO z_pants_musinsa VALUES(null,%s,%s,%s,%s,%s,%s,%s)", row)
 	cursor.execute("UPDATE z_pants_musinsa SET z_pants_musinsa.Pantsno=substr(link,45,6);")
+	cursor.execute("ALTER TABLE z_pants_musinsa ADD CONSTRAINT z_pants_musinsa_PK_PantsNoandSize PRIMARY KEY(PantsNo,Size);")
+	cursor.execute("ALTER TABLE z_pants_musinsa ADD CONSTRAINT z_pants_musinsa_FK_PantsNo FOREIGN KEY(PantsNo) REFERENCES z_table1(PantsNo)")
 	#for r in cursor:
 	#    print(r)		
 	conn.commit()
