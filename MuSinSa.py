4a4200d30803247c540b3b1784e797ebdea14e88
ndgndg91 <ndgndg91@gmail.com>
2016-11-13 22:07:04 +0900

    moreprecise

--- MuSinSa.py
+++ MuSinSa.py
@@ -5,7 +5,7 @@ import re
 import csv
 
 def spider(max_pages):
-	page = 358864
+	page = 302000
 	fixpage = page
 	while page < max_pages:
 		link='http://store.musinsa.com/app/product/detail/'+str(page)+'/0'
@@ -14,7 +14,8 @@ def spider(max_pages):
 		soup=BeautifulSoup(f,'lxml')
 		a = soup.find_all('p',{'class':'item_categories'})
 		b = bool(re.search('하의',str(a)))
-		if b == True:
+		c = bool(soup.find('table',class_='table_th_grey'))
+		if b == True and c == True:
 			spanspan =soup.find_all('span',class_='product-img')
 			span = str(spanspan)
 			
@@ -27,13 +28,14 @@ def spider(max_pages):
 			#print(span)
 			q = re.compile('image.musinsa.com/images/goods_img/[0-9]{8}/'+str(page)+'/'+str(page)+'_[0-9]_[0-9]{3}.jpg')
 			imrl = re.search(q,span)
+			imgurl = []
 			imgurlsource = imrl.group()
 			#print(imgurlsource)
-			imgurl = []
 			imgurl.append(imgurlsource)
 			#print(imgurl)
 
-
+			
+			
 			all_table=soup.find('table',class_='table_th_grey')
 			theadhead=str(all_table.find('thead'))
 			tbodybody=str(all_table.find('tbody'))
@@ -71,10 +73,11 @@ def spider(max_pages):
 				#print(rows)
 				row1 = rows[0:6]
 				row1.append(link); row1.append(pantsno)
-				row2 = rows[6:12]
-				row2.append(link); row2.append(pantsno)
 				print(row1)
-				print(row2)
+				if len(rows)>6:
+					row2 = rows[6:12]
+					row2.append(link); row2.append(pantsno)
+					print(row2)
 				if len(rows)>12:
 					row3 = rows[12:18]
 					row3.append(link); row3.append(pantsno)
@@ -95,11 +98,13 @@ def spider(max_pages):
 		
 				#print(link)
 				page+=1
-				outfile = open(str(fixpage)+'~'+str(max_pages-1)+'table2.csv','a',encoding='utf-8') 
+				outfile = open('table2.csv','a',encoding='utf-8')
+				#outfile = open(str(fixpage)+'~'+str(max_pages-1)+'table2.csv','a',encoding='utf-8')
 				write_outfile = csv.writer(outfile)
 				#write_outfile.writeheader(headers)
 				write_outfile.writerow(row1)
-				write_outfile.writerow(row2)
+				if len(rows)>6:
+					write_outfile.writerow(row2)
 				if len(rows)>12:
 					write_outfile.writerow(row3)
 				if len(rows)>18:
@@ -117,12 +122,16 @@ def spider(max_pages):
 				table1_row.append(pantsno)
 				print(table1_row)
 
-				outfile2 = open(str(fixpage)+'~'+str(max_pages-1)+'table1.csv','a',encoding='utf-8')
+				outfile2 = open('table1.csv','a',encoding='utf-8')
+				#outfile2 = open(str(fixpage)+'~'+str(max_pages-1)+'table1.csv','a',encoding='utf-8')
 				write_outfile2 = csv.writer(outfile2)
 				write_outfile2.writerow(table1_row)
 				outfile2.close()
+			else:
+				print("Column is not matching,, Maybe This is not pants?")
+				page+=1
 		else:
 			print("This page is not about pants and i'm gonna scrap next page")
 			page+=1
 		
-spider(358870)
+spider(305000)
