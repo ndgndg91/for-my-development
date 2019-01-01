77bc45b41c458dbcc7c750f8eb2e38e78334bd76
ndgndg91 <ndgndg91@gmail.com>
2019-01-01 19:13:29 +0900

    Number

---
+++ ES6/_chapter9.Number/epsilon.js
@@ -0,0 +1,11 @@
+
+//1보다 큰 값에서 최솟값과 1과의 차이를 나타낸다.
+//유동 소수점 처리에서 미세한 값 차이 때문에 일치하지 않을 때 사용.
+let total = 0.1 + 0.2;
+console.log(total);
+
+let result = (Math.abs(0.1 + 0.2 - 0.3) < Number.EPSILON);
+console.log(result);
+
+let value = (Math.pow(10,1) * 0.1) + (Math.pow(10,1) * 0.2);
+console.log(value / 10 === 0.3);
