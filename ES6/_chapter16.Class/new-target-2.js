b77f1e33ef2c13d32c73213546978f0e2a18462d
ndgndg91 <ndgndg91@gmail.com>
2019-02-09 00:11:37 +0900

    target

--- ES6/_chapter16.Class/new-target-2.js
+++ ES6/_chapter16.Class/new-target-2.js
@@ -13,9 +13,9 @@ class Soccer extends Sports{
 
 let sportsObj = new Sports();
 let soccerObj = new Soccer();
-//Sports: Sporst
-//Sports: Soccer
-//Sports: Soccer
+//Sports: Sporst              --> Sports 생성자
+//Sports: Soccer              --> Sports 생성자
+//Soccer: Soccer              --> Soccer 생성자
 //ES6에서 겉으로 드러나지 않는 프로퍼티가 있는데 name 프로퍼티이다.
 //ES5에도 name 프로퍼티가 있었는데, 값이 설정되지 않아서 그다지 사용하지 않았다.
 //ES6는 클래스, 함수, 오브젝트에 name 프로퍼티가 존재하며 이름이 설정된다.
