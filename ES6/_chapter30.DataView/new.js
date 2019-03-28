db334d949f237a000d259bde2d61be9552ab8049
ndgndg91 <ndgndg91@gmail.com>
2019-03-28 19:56:57 +0900

    omission

--- ES6/_chapter30.DataView/new.js
+++ ES6/_chapter30.DataView/new.js
@@ -7,7 +7,7 @@ console.log(viewObj.byteOffset);//0
 console.log(viewObj.byteLength);//12
 
 let bufferObj2 = new ArrayBuffer(12);
-let viewObj = new DataView(bufferObj, 1, 8);
+let viewObj2 = new DataView(bufferObj2, 1, 8);
 
-console.log(viewObj.byteOffset);
-console.log(viewObj.byteLength);
+console.log(viewObj2.byteOffset);//1
+console.log(viewObj2.byteLength);//8
