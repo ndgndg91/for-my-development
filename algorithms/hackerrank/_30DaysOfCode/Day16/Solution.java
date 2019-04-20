2857055b725c7776d5a290dead00068b1b3c3a55
ndgndg91 <ndgndg91@gmail.com>
2019-04-20 16:46:30 +0900

    hackerRank

---
+++ algorithms/hackerrank/_30DaysOfCode/Day16/Solution.java
@@ -0,0 +1,17 @@
+package hackerrank._30DaysOfCode.Day16;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try {
+            System.out.println(Integer.parseInt(S));
+        } catch (Exception e){
+            System.out.println("Bad String");
+        }
+    }
+}
+
